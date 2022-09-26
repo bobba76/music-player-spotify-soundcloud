3989f1a60e2f7eeae9960d316b57b4a38a1b0a92
Robin Lindgren <rlindgren8@gmail.com>
2022-09-26 21:27:40 +0200

    Changed Angular Project name to player-web

--- player/karma.conf.js
+++ player/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/player'),
+      dir: require('path').join(__dirname, './coverage/player-web'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- player/src/app/app.component.spec.ts
+++ player/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'player'`, () => {
+  it(`should have as title 'player-web'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('player');
+    expect(app.title).toEqual('player-web');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('player app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('player-web app is running!');
   });
 });
